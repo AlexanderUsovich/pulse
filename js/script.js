88a1baaa6b271e3eb50182b634e7f28b3dbed2ff
Alex <usovich_al@mail.ru>
2022-01-25 22:46:45 +0300

    tabs

--- js/script.js
+++ js/script.js
@@ -4,31 +4,39 @@ $(document).ready(function () {
         adaptiveHeight: false,
         prevArrow: '<button type="button" class="slick-prev"><img src="img/Slide/Icons/left.png" alt="" /></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="img/Slide/Icons/right.png" alt="" /></button>',
-        slidesToShow: 3,
-        slidesToScroll: 3,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
         responsive: [
             {
                 breakpoint: 992,
                 settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 2,
-                    infinite: true,
                     dots: true,
                     arrows: false
                 }
             },
-            {
-                breakpoint: 780,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1,
-                    dots: true,
-                    arrows: false
-                }
-            }
             // You can unslick at a given breakpoint now by adding:
             // settings: "unslick"
             // instead of a settings object
         ]
     });
+
+    $('.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function () {
+        $(this)
+            .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+            .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+    });
+
+    function toggleClass(item){
+        $(item).each(function (i) {
+            $(this).on('click', function (e) {
+                e.preventDefault();
+                $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+                $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+            })
+        });
+    }
+    toggleClass('.catalog-item__link');
+    toggleClass('.catalog-item__back');
+
 });
